74c38de5e74c482e5b6bdc0f712d553ad2ef4b7a
dima <vds788502@gmail.com>
2025-03-29 02:02:47 +0400

    Объединить все функции работы с SQL водин класс DataBase

--- sql_requests.py
+++ sql_requests.py
@@ -3,60 +3,66 @@ from pathlib import Path
 from sqlite3 import connect
 
 
+class DataBase:
+    def __init__(self, name_db):
+        with open("config.json") as data:
+            information = load(data)
+            self.__PATH_TO_DB = Path(information["data_base_path"])
+            self.__NAME_MAIN_TABLE = information["name_main_table"]
 
-with open("config.json") as data:
-    information = load(data)
-    PATH_TO_DB = Path(information["data_base_path"])
-    NAME_MAIN_TABLE = information["name_main_table"]
+        self.create_main_table()
 
-
-def create_main_table(name_db=NAME_MAIN_TABLE):
-    db = connect(PATH_TO_DB)
-    curr = db.cursor()
-    curr.execute(f"""
-        CREATE TABLE IF NOT EXISTS {name_db} (
-            "id"	INTEGER NOT NULL UNIQUE,
-            "ads"   INTEGER NOT NULL UNIQUE,  
-            PRIMARY KEY("id" AUTOINCREMENT)
-        );
-        """)
-    db.commit()
-    db.close()
-
-
-def new_information(id_ads: list[int]):
-    db = connect(PATH_TO_DB)
-    curr = db.cursor()
-    try:
-        last_id = get_information()
-        for id_el in id_ads:
-            if id_el in last_id:
-                continue
-            curr.execute(f"""
-                INSERT INTO {NAME_MAIN_TABLE} (ads) VALUES
-                    (?)
-            """, (id_el,))
-    except Exception as ex:
-        print(ex)
-    finally:
-        db.commit()
-        db.close()
-
-
-# noinspection PyBroadException
-def get_information() -> set[int]:
-    db = connect(PATH_TO_DB)
-    curr = db.cursor()
-    try:
+    def create_main_table(self):
+        db = connect(self.__PATH_TO_DB)
+        curr = db.cursor()
         curr.execute(f"""
-            SELECT ads
-            FROM {NAME_MAIN_TABLE}
-        """)
-        return set(map(lambda el: el[0], curr.fetchall()))
-    except:
-        res = set()
-        res.add(-1)
-        return res
-    finally:
+            CREATE TABLE IF NOT EXISTS {self.__NAME_MAIN_TABLE} (
+                "id"	INTEGER NOT NULL UNIQUE,
+                "ads"   INTEGER NOT NULL UNIQUE,  
+                PRIMARY KEY("id" AUTOINCREMENT)
+            );
+            """)
         db.commit()
         db.close()
+
+    def new_information(self, id_ads: list[int]):
+        db = connect(self.__PATH_TO_DB)
+        curr = db.cursor()
+        try:
+            last_id = self.get_information()
+            for id_el in id_ads:
+                if id_el in last_id:
+                    continue
+                curr.execute(f"""
+                    INSERT INTO {self.__NAME_MAIN_TABLE} (ads) VALUES
+                        (?)
+                """, (id_el,))
+        except Exception as ex:
+            print(ex)
+        finally:
+            db.commit()
+            db.close()
+
+    # noinspection PyBroadException
+    def get_information(self) -> set[int]:
+        db = connect(self.__PATH_TO_DB)
+        curr = db.cursor()
+        try:
+            curr.execute(f"""
+                SELECT ads
+                FROM {self.__NAME_MAIN_TABLE}
+            """)
+            return set(map(lambda el: el[0], curr.fetchall()))
+        except:
+            res = set()
+            res.add(-1)
+            return res
+        finally:
+            db.commit()
+            db.close()
+
+    def get_path_db(self):
+        return self.__PATH_TO_DB
+
+    def get_name_table(self):
+        return self.__NAME_MAIN_TABLE
