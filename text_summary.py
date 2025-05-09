758395cb5b84e606716d60e4af79b205dd1c51e1
dima <vds788502@gmail.com>
2025-05-09 18:34:53 +0400

    возвращает уже обработанный текст

--- text_summary.py
+++ text_summary.py
@@ -1,14 +1,13 @@
-import nltk
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.text_rank import TextRankSummarizer
 
 def summy_message(text):
-    nltk.download('punkt_tab')
-
     parser = PlaintextParser.from_string(text, Tokenizer("russian"))
     summarizer = TextRankSummarizer()
 
-    summary = summarizer(parser.document, 3)  # получить 2 предложения
-
-    return summary
+    summary = summarizer(parser.document, 3)
+    text_ads = ""
+    for sentence in summary:
+        text_ads += str(sentence) + "\n"
+    return text_ads
